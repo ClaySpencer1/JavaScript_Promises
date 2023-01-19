032574dec36a0d1ef531365f71358d2ae106aa3d
Clay Spencer <clayspencer52@gmail.com>
2023-01-19 14:43:10 -0600

    doneski

--- app.js
+++ app.js
@@ -16,9 +16,28 @@ function getList() {
 }
 
 // TODO: Handle the resolved or rejected states of the promise
+let pError = document.getElementById("error");
+let ul = document.getElementById("list");
 
+let promise = getList();
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+function listContainer(list){
+  list.forEach(hobbit => {
+    let li = document.createElement("li")
+    li.textContent = hobbit;
+    ul.appendChild(li);
+  });
+}
+
 
 // TODO: If the promise rejects with the failure object
+
+function errorContainer(error){
+  console.error(error);
+  pError.textContent = error.message;
+}
 // Display the failure message in the paragraph element with id="error" (check index.html file)
+promise.then(listContainer).catch(error);
+
+//I did mine a little different from Hunter's example but it is completely functional
